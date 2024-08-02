280868ceaf34db749eaf57a8557511080e58fc3d
SahanDev00 <Dumidus86@gmail.com>
2024-08-02 09:55:28 +0530

    commited

--- src/Components/DarkMode.jsx
+++ src/Components/DarkMode.jsx
@@ -2,35 +2,35 @@ import React, { useEffect, useState } from 'react';
 import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
 
 const DarkMode = () => {
-    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark'); // Default to dark mode
 
-    useEffect(() => {
-        const element = document.documentElement;
-        if (theme === 'dark') {
-            element.classList.add('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            element.classList.remove('dark');
-            localStorage.setItem('theme', 'light');
-        }
-    }, [theme]);
+  useEffect(() => {
+    const element = document.documentElement;
+    if (theme === 'dark') {
+      element.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      element.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    }
+  }, [theme]);
 
-    const changeTheme = () => {
-        setTheme(theme === 'light' ? 'dark' : 'light');
-    };
+  const changeTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
 
-    return (
-        <div className='relative'>
-            <MdOutlineLightMode size={25}
-                onClick={changeTheme}
-                className={`w-12 absolute drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] cursor-pointer ${theme !== 'dark' ? 'opacity-0' : 'opacity-100 text-white'}`}
-            />
-            <MdDarkMode size={25}
-                onClick={changeTheme}
-                className={`w-12 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] cursor-pointer ${theme !== 'dark' ? 'opacity-100' : 'opacity-0'}`}
-            />
-        </div>
-    );
+  return (
+    <div className='relative'>
+      <MdOutlineLightMode size={25}
+        onClick={changeTheme}
+        className={`w-12 absolute drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] cursor-pointer ${theme !== 'dark' ? 'opacity-0' : 'opacity-100 text-white'}`}
+      />
+      <MdDarkMode size={25}
+        onClick={changeTheme}
+        className={`w-12 drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] cursor-pointer ${theme !== 'dark' ? 'opacity-100' : 'opacity-0'}`}
+      />
+    </div>
+  );
 };
 
 export default DarkMode;
