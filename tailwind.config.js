ceeddb0676799689959fe2048d41962fd9d5b58f
SahanDev00 <Dumidus86@gmail.com>
2024-08-02 09:41:08 +0530

    commited

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const flowbite = require("flowbite-react/tailwind");
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     flowbite.content(),
