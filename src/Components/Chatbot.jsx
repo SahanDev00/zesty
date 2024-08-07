ed4ccac531158be23e9bcf7e4078c50523cacbc3
SahanDev00 <Dumidus86@gmail.com>
2024-08-07 13:38:41 +0530

    commited

--- src/Components/Chatbot.jsx
+++ src/Components/Chatbot.jsx
@@ -19,7 +19,7 @@ const Chatbot = () => {
   }, []);
 
   return (
-    <div className='w-[90%] sm:w-[85%] md:w-[60%] lg:w-[70%] h-[70vh] sm:h-[70vh] md:h-[70vh] lg:h-[70vh] xl:h-[60vh] mx-auto mt-20 mb-20 ' id="myLandbot"></div>
+    <div className='w-[90%] sm:w-[85%] md:w-[60%] lg:w-[70%] h-[70vh] sm:h-[70vh] md:h-[70vh] lg:h-[70vh] xl:h-[60vh] mx-auto mt-20 mb-20' id="myLandbot"></div>
   );
 }
 
